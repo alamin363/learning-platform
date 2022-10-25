196a465c21bbfaf151c5303d2bce3d6b5a4a8f91
alaminpk360 <alaminpkalaminpk5@gmail.com>
2022-10-25 22:38:15 +0600

    added nav

--- src/component/Form/Login.js
+++ src/component/Form/Login.js
@@ -1,9 +1,38 @@
-import React from 'react';
-import './Login.css'
+import React from "react";
+import "./Login.css";
+import { FaGoogle, FaGithub, FaLock, FaMailBulk } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const Login = () => {
+
+  const handelSubmit = event =>{
+    event.preventDefault()
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+  }
   return (
     <div>
-      <button></button>
+     <div className="online-login">
+     <button className="btn-fblogin">
+        <FaGoogle style={{fontSize:"15px", color:"green"}}/> <span className="text">Login With Google</span>
+      </button>
+      <button className="btn-fblogin">
+        <FaGithub style={{fontSize:"15px", color:"black"}}/> <span className="text">Login With Google</span>
+      </button>
+      <form onSubmit={handelSubmit}>
+      <div className="input-feld">
+      <input name="email" placeholder="Email" type="email" required/>
+      <span className="email"><FaMailBulk style={{color:"gray"}}/></span>
+      </div>
+      <div className="input-feld">
+      <input name="password" placeholder="Password" type="password" />
+      <span className="email"><FaLock style={{color:"gray"}}/></span>
+      </div>
+      <button className="btn">Log In</button>
+      </form>
+      <p>or <Link>Forget Password</Link></p>
+      <p>Don't have an account? <Link to="/register">Sing Up</Link></p>
+     </div>
     </div>
   );
 };
--- src/component/Form/Register.js
+++ src/component/Form/Register.js
@@ -1,9 +1,26 @@
 import React from 'react';
-
+import { FaLock, FaMailBulk } from 'react-icons/fa';
+import './Register.css'
 const Register = () => {
+  const handelSubmit = event =>{
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+
+
+  }
   return (
     <div>
-      <h1>this is Register form</h1>
+      <form onSubmit={handelSubmit}>
+      <input name="name" placeholder="Full Name" type="text" />
+      <input name="email" placeholder="Email" type="email" required/>
+      <input name="password" placeholder="Password" type="password" />
+      <div className="underline">
+      </div>
+      <button className="btn">Log In</button>
+      </form>
     </div>
   );
 };
