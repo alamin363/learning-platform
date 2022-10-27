83068f1f77dd490529ec21178e50afeb0dfec81e
alaminpk360 <alaminpkalaminpk5@gmail.com>
2022-10-27 12:34:54 +0600

    styling heder

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-      
+      <div>
+        <Link>About us</Link>
+        <Link>Get the app</Link>
+        <Link>Countact us</Link>
+        <Link>Careers</Link>
+        <Link>Blog</Link>
+        <Link>Help</Link>
+        <Link>Help And Support</Link>
+        <Link>Investors</Link>
+        <Link>Terms</Link>
+        <Link>Privacy Policy</Link>
+        <Link>Cookie Settings</Link>
+
+        <h1>We Are a Programer 🧟</h1>
+      </div>
     </div>
   );
 };
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './Header.css'
-import Book from '../../Utility/download.jpg'
+import Book from '../../Utility/coder.jpg'
 const Header = () => {
   return (
-    <div>
       <div className='container'>
-        
+       {/* <img src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPYTZlc0oTmUmhbfhitREOpB0npameCO6MHA&usqp=CAU`} alt="" /> */}
+       <img src={Book} alt="" />
       </div>
-    </div>
 
   );
 };
